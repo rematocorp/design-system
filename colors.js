352e02c3fa6cf229b201594e34cac08892e49830
Uku Pattak <uku.pattak@remato.ee>
2021-01-31 11:43:36 +0200

    Turn colors into much more
    - Add border radius variables
    - Add JS colors
    - Rename package

---
+++ colors.js
@@ -0,0 +1,48 @@
+/**
+ * Remato color palette
+ *
+ * https=//www.figma.com/file/IpGgFUV8d7JPviRf65mWZ2c5/COLORS
+ *
+ * NB! Make sure you update colors.scss as well.
+ */
+
+export const black = '#10191f'
+export const white = '#fff'
+
+export const grey900 = '#21292f'
+export const grey800 = '#2a3339'
+export const grey700 = '#40474f'
+export const grey600 = '#6e757c'
+export const grey400 = '#a7b0b6'
+export const grey300 = '#ccd5db'
+
+export const blueGrey300 = '#c2d6e5'
+export const blueGrey200 = '#d1e2ef'
+export const blueGrey100 = '#e8f0f7'
+export const blueGrey50 = '#f3f8fb'
+
+export const green700 = '#19860b'
+export const green600 = '#33a025'
+export const green500 = '#4cb93e'
+export const green200 = '#d2edcf'
+export const green100 = '#edf8ec'
+
+export const blue700 = '#0053c2'
+export const blue600 = '#006ddc'
+export const blue500 = '#0e86f5'
+export const blue200 = '#c3e1fc'
+export const blue100 = '#e7f3fe'
+
+export const red700 = '#ac1800'
+export const red600 = '#c63209'
+export const red500 = '#df4b22'
+export const red200 = '#f7d2c8'
+export const red100 = '#fcede9'
+
+export const orange700 = '#bb7a00'
+export const orange600 = '#d59400'
+export const orange500 = '#eead09'
+
+export const yellow500 = '#f7da61'
+export const yellow200 = '#fdf6d7'
+export const yellow100 = '#fefbef'
